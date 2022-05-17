b133f475f9d9485790ae73a5152b3697c4f59ef4
alexsherry21 <sherrodactyl@yahoo.com>
2022-05-17 18:52:13 -0400

    initial files and table

---
+++ static/js/app.js
@@ -0,0 +1,24 @@
+// import the data from data.js
+const tableData = data;
+
+//reference the html table using d3
+var tbody = d3.select("tbody");
+
+function buildTable(data) {
+    //clear any existing data
+    tbody.html("");
+
+    //loop through each object in the data
+    data.forEach((dataRow) => {
+        //append a row to table body
+        let row = tbody.append("tr");
+
+        //loop through each field in the dataRow 
+        //and add each value as a table cell (td)
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+        }
+    );
+});
+};
